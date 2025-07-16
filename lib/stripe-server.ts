7d5ed779788d34217320b999d9340fb2002eb177
hongwei.he <hongwei.he@sparkedu.com>
2025-07-16 16:09:28 +0800

    fix: 测试

--- lib/stripe-server.ts
+++ lib/stripe-server.ts
@@ -1,11 +1,11 @@
-import 'server-only'
-import Stripe from 'stripe'
+import "server-only";
+import Stripe from "stripe";
 
 if (!process.env.STRIPE_SECRET_KEY) {
-  throw new Error('STRIPE_SECRET_KEY is not set')
+  throw new Error("STRIPE_SECRET_KEY is not set");
 }
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2025-05-28.basil',
+  apiVersion: "2025-06-30.basil",
   typescript: true,
-}) 
+});
